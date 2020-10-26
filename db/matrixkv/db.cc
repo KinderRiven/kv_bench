82b9f84c11fdadc82bfbc9259d466b948092b92b
K <1339764596@qq.com>
2020-10-26 21:00:00 +0800

    push by hsk

--- db/matrixkv/db.cc
+++ db/matrixkv/db.cc
@@ -1,11 +1,11 @@
 #include "db.h"
-#include "rocksdb.h"
+#include "matrixkv.h"
 
 using namespace kv_benchmark;
 
 void DB::Open(Options& options, DB** dbptr)
 {
-    *dbptr = new RocksDB(options);
+    *dbptr = new MatrixKV(options);
 }
 
 void DB::Close(DB* dbptr)
