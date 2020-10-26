f5230f6963a27c16dadc7c4e3687491a6f7e09ce
K <1339764596@qq.com>
2020-10-26 16:22:22 +0800

    push by hsk

--- db/leveldb/leveldb.cc
+++ db/leveldb/leveldb.cc
@@ -5,6 +5,7 @@ using namespace kv_benchmark;
 LevelDB::LevelDB(kv_benchmark::Options& options)
 {
     leveldb::Options _options;
+    _options.create_if_missing = true;
     leveldb::DB::Open(_options, options.db_path, &db_);
     assert(db_ != nullptr);
 }
