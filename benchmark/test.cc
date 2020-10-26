7b855fd6b07a4475ac2685e370baee3dd9cdc74d
K <1339764596@qq.com>
2020-10-26 16:16:36 +0800

    push by hsk

--- benchmark/test.cc
+++ benchmark/test.cc
@@ -1,4 +1,5 @@
 #include "db.h"
+#include "header.h"
 #include "options.h"
 
 using namespace kv_benchmark;
@@ -8,5 +9,16 @@ int main(int argc, char** argv)
     DB* _db;
     Options _options;
     DB::Open(_options, &_db);
+
+    char _key[128];
+    char _value[256];
+
+    for (int i = 0; i < 100000; i++) {
+        sprintf(_key, "%llu", i);
+        sprintf(_value, "%llu", i);
+        _db->Put(_key, 16, _value, 128);
+    }
+
+    DB::Close(_db);
     return 0;
 }
---
+++ gitpush.sh
@@ -0,0 +1,8 @@
+git add benchmark/*.cc
+git add benchmark/Makefile
+git add db
+git add include
+git add third-party/*.zip
+git add gitpush.sh
+git commit -m "push by hsk"
+git push
