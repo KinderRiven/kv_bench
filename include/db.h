af30e4fbfdea2c88b0d80eb9e6bfa6e66e60b8cd
K <1339764596@qq.com>
2020-10-26 16:19:52 +0800

    push by hsk

--- include/db.h
+++ include/db.h
@@ -15,21 +15,21 @@ public:
     // Put key-value pair into database.
     // If "key" already exists, it will return false,
     // returns OK on success, and a non-OK status on error.
-    bool Put(char* key, size_t key_length, char* value, size_t value_length);
+    virtual bool Put(char* key, size_t key_length, char* value, size_t value_length) = 0;
 
     // Update key-value pair which has existed in database,
     // If "key" not exists, it will return false.
-    bool Update(char* key, size_t key_length, char* value, size_t value_length);
+    virtual bool Update(char* key, size_t key_length, char* value, size_t value_length) = 0;
 
 public:
     // Get key-value pair from database.
-    bool Get(char* key, size_t key_length, char* value, size_t& value_length);
+    virtual bool Get(char* key, size_t key_length, char* value, size_t& value_length) = 0;
 
     // From key to get <scan_count> key-value pairs.
-    bool Scan(char* key, size_t key_length, size_t scan_count);
+    virtual bool Scan(char* key, size_t key_length, size_t scan_count) = 0;
 
 public:
-    void Print();
+    virtual void Print() = 0;
 };
 };
 
