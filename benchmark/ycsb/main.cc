02a0710713237842164eb5bbdd3134ed71e57abf
K <1339764596@qq.com>
2020-11-02 13:24:33 +0800

    push by hsk

--- benchmark/ycsb/main.cc
+++ benchmark/ycsb/main.cc
@@ -4,6 +4,43 @@
 #include "workload_generator.h"
 #include "ycsb.h"
 
+struct workload_options {
+public:
+    int type;
+    char name[256];
+
+public:
+    std::string result_path;
+    int num_threads;
+    size_t key_length;
+    size_t value_length;
+
+public:
+    uint64_t dbsize;
+    size_t workload_size;
+    kv_benchmark::DB* db;
+};
+
+static void start_workload(struct workload_options* options)
+{
+    uint64_t _key_base = 1;
+    kv_benchmark::YCSB* _benchmarks[32];
+    size_t _range = (options->dbsize / options->value_length);
+
+    for (int i = 0; i < options->num_threads; i++) {
+        _benchmarks[i] = new kv_benchmark::YCSB(options->type, _key_base, _range, options->key_length, options->value_length);
+    }
+
+    kv_benchmark::generator_parameter _gparam;
+    _gparam.key_length = options->key_length;
+    _gparam.value_length = options->value_length;
+    _gparam.num_threads = options->num_threads;
+    _gparam.data_size = (options->workload_size / options->num_threads);
+
+    kv_benchmark::WorkloadGenerator* _run = new kv_benchmark::WorkloadGenerator(options->name, &_gparam, options->db, _benchmarks);
+    _run->Run();
+}
+
 int main(int argc, char* argv[])
 {
     // DB opation
@@ -11,23 +48,27 @@ int main(int argc, char* argv[])
     char _pmem_path[128] = "/home/pmem0";
 
     // Workload Parameter
-    int _num_thread = 1;
+    int _num_threads = 1;
     size_t _key_length = 16;
     size_t _value_length = 1000;
-    size_t _data_size = 1024 * 1024 * 1024;
+    size_t _dbsize = 1024 * 1024 * 1024;
+    double _psize = 0.2;
 
     // workload generator
     for (int i = 0; i < argc; i++) {
         char junk;
         uint64_t n;
+        double f;
         if (sscanf(argv[i], "--key_length=%llu%c", &n, &junk) == 1) {
             _key_length = n;
         } else if (sscanf(argv[i], "--value_length=%llu%c", &n, &junk) == 1) {
             _value_length = n;
         } else if (sscanf(argv[i], "--num_thread=%llu%c", &n, &junk) == 1) {
-            _num_thread = n;
-        } else if (sscanf(argv[i], "--size=%llu%c", &n, &junk) == 1) { // GB
-            _data_size = n * (1024 * 1024 * 1024);
+            _num_threads = n;
+        } else if (sscanf(argv[i], "--dbsize=%llu%c", &n, &junk) == 1) { // GB
+            _dbsize = n * (1024 * 1024 * 1024);
+        } else if (sscanf(argv[i], "--psize=%lf%c", &f, &junk) == 1) { // GB
+            _psize = f;
         } else if (strncmp(argv[i], "--nvm=", 6) == 0) {
             strcpy(_pmem_path, argv[i] + 6);
         } else if (strncmp(argv[i], "--ssd=", 6) == 0) {
@@ -44,59 +85,36 @@ int main(int argc, char* argv[])
     _options.db_path.assign(_ssd_path);
     kv_benchmark::DB::Open(_options, &_db);
 
-    kv_benchmark::generator_parameter _gparam;
-    _gparam.key_length = _key_length;
-    _gparam.value_length = _value_length;
-    _gparam.num_threads = _num_thread;
-    _gparam.data_size = _data_size;
-
     // CREATE RESULT SAVE PATH
     time_t _t = time(NULL);
     struct tm* _lt = localtime(&_t);
     char _result_path[128];
     sprintf(_result_path, "%04d%02d%02d_%02d%02d%02d", 1900 + _lt->tm_year, _lt->tm_mon, _lt->tm_mday, _lt->tm_hour, _lt->tm_min, _lt->tm_sec);
     mkdir(_result_path, 0777);
-    _gparam.result_path.assign(_result_path);
 
-    // SEQ WARMUP
-    uint64_t _key_base = 1;
-    kv_benchmark::YCSB* _benchmarks[32];
-    size_t _range = _data_size / (_num_thread * _value_length);
-    for (int i = 0; i < _num_thread; i++) {
-        _benchmarks[i] = new kv_benchmark::YCSB(YCSB_SEQ_LOAD, _key_base, _range, _key_length, _value_length);
-        _key_base += _range;
-    }
-    kv_benchmark::WorkloadGenerator* _warmup = new kv_benchmark::WorkloadGenerator("SEQ_WARMUP", &_gparam, _db, _benchmarks);
-    _warmup->Run();
+    struct workload_options _wopt;
+    _wopt.key_length = _key_length;
+    _wopt.value_length = _value_length;
+    _wopt.num_threads = _num_threads;
+    _wopt.dbsize = _dbsize;
+    _wopt.db = _db;
+    _wopt.result_path.assign(_result_path);
+    _wopt.workload_size = (size_t)(_psize * _dbsize);
 
-    // YCSB-A
-    _key_base = 1;
-    _range = _data_size / (_num_thread * _value_length);
-    for (int i = 0; i < _num_thread; i++) {
-        _benchmarks[i] = new kv_benchmark::YCSB(YCSB_A, _key_base, _range, _key_length, _value_length);
-        _key_base += _range;
-    }
-    _warmup = new kv_benchmark::WorkloadGenerator("YCSB_A", &_gparam, _db, _benchmarks);
-    _warmup->Run();
+    strcpy(_wopt.name, "WARMUP");
+    _wopt.type = YCSB_SEQ_LOAD;
+    start_workload(&_wopt);
 
-    // YCSB-C (0)
-    _key_base = 1;
-    _range = _data_size / (_num_thread * _value_length);
-    for (int i = 0; i < _num_thread; i++) {
-        _benchmarks[i] = new kv_benchmark::YCSB(YCSB_C, _key_base, _range, _key_length, _value_length);
-        _key_base += _range;
-    }
-    _warmup = new kv_benchmark::WorkloadGenerator("YCSB_C-0", &_gparam, _db, _benchmarks);
-    _warmup->Run();
+    strcpy(_wopt.name, "YCSB_A");
+    _wopt.type = YCSB_A;
+    start_workload(&_wopt);
 
-    // YCSB-C (1)
-    _key_base = 1;
-    _range = _data_size / (_num_thread * _value_length);
-    for (int i = 0; i < _num_thread; i++) {
-        _benchmarks[i] = new kv_benchmark::YCSB(YCSB_C, _key_base, _range, _key_length, _value_length);
-        _key_base += _range;
-    }
-    _warmup = new kv_benchmark::WorkloadGenerator("YCSB_C-1", &_gparam, _db, _benchmarks);
-    _warmup->Run();
+    strcpy(_wopt.name, "YCSB_C-0");
+    _wopt.type = YCSB_C;
+    start_workload(&_wopt);
+
+    strcpy(_wopt.name, "YCSB_C-1");
+    _wopt.type = YCSB_C;
+    start_workload(&_wopt);
     return 0;
 }
