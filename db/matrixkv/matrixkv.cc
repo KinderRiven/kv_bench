754375305550d2faac75f8b84b3b1cb3f339968c
K <1339764596@qq.com>
2020-11-04 16:32:08 +0800

    push by hsk

--- db/matrixkv/matrixkv.cc
+++ db/matrixkv/matrixkv.cc
@@ -1,4 +1,5 @@
 #include "matrixkv.h"
+#include "rocksdb/table.h"
 
 using namespace kv_benchmark;
 
@@ -12,6 +13,16 @@ MatrixKV::MatrixKV(kv_benchmark::Options& options)
     _nvm_opt->pmem_path = "/home/pmem0";
     _nvm_opt->use_nvm_module = true;
     _options.nvm_setup.reset(_nvm_opt);
+
+    // set block cache
+    std::shared_ptr<rocksdb::Cache> _cache = rocksdb::NewLRUCache(8 * 1024 * 1024);
+    rocksdb::BlockBasedTableOptions _table_options;
+    _table_options.block_cache = _cache;
+    _options.table_factory.reset(NewBlockBasedTableFactory(_table_options));
+
+    // direct read/write
+    _options.use_direct_reads = true;
+    _options.use_direct_io_for_flush_and_compaction = true;
 
     rocksdb::DB::Open(_options, options.db_path, &db_);
     assert(db_ != nullptr);
