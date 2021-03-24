580f4d89e000bb869f830a565f4d5600ba57705a
KinderRiven <1339764596@qq.com>
2021-03-24 16:16:20 +0800

    push by hsk

--- benchmark/ycsb/run.sh
+++ benchmark/ycsb/run.sh
@@ -1,14 +1,23 @@
 ###
  # @Author: your name
  # @Date: 2021-03-24 15:38:05
- # @LastEditTime: 2021-03-24 16:06:42
+ # @LastEditTime: 2021-03-24 16:09:46
  # @LastEditors: Please set LastEditors
  # @Description: In User Settings Edit
  # @FilePath: /kv_bench/benchmark/run.sh
 ### 
 
-APP=./rocksdb_tester
-APP=./matrixkv_tester
-APP=./silk_tester
+APP1=./rocksdb_tester
+$APP1 --btype=warmup --nvm=/home/pmem0 --ssd=/home/hanshukai/mount/4510/db --num_thread=4 --dbsize=100 --psize=0.5
+$APP1 --btype=warmup --nvm=/home/pmem0 --ssd=/home/hanshukai/mount/4510/db --num_thread=4 --dbsize=100 --psize=0.5
+rm -rf /home/hanshukai/mount/4510/db
 
-$APP --btype=warmup --nvm=/home/pmem0 --ssd=/home/hanshukai/mount/4510/db --num_thread=4 --dbsize=100 --psize=0.5
+APP2=./silk_tester
+$APP2 --btype=warmup --nvm=/home/pmem0 --ssd=/home/hanshukai/mount/4510/db --num_thread=4 --dbsize=100 --psize=0.5
+$APP2 --btype=warmup --nvm=/home/pmem0 --ssd=/home/hanshukai/mount/4510/db --num_thread=4 --dbsize=100 --psize=0.5
+rm -rf /home/hanshukai/mount/4510/db
+
+APP3=./matrixkv_tester
+$APP3 --btype=warmup --nvm=/home/pmem0 --ssd=/home/hanshukai/mount/4510/db --num_thread=4 --dbsize=100 --psize=0.5
+$APP3 --btype=warmup --nvm=/home/pmem0 --ssd=/home/hanshukai/mount/4510/db --num_thread=4 --dbsize=100 --psize=0.5
+rm -rf /home/hanshukai/mount/4510/db
