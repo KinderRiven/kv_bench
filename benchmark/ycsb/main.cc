cd0188d9ad563af530167b46600ec5aefa4b059d
KinderRiven <1339764596@qq.com>
2021-03-26 10:58:51 +0800

    push by hsk

--- benchmark/ycsb/main.cc
+++ benchmark/ycsb/main.cc
@@ -171,7 +171,7 @@ int main(int argc, char* argv[])
         strcpy(_wopt.name, "WARMUP");
         _wopt.type = YCSB_RANDOM_LOAD | g_zipf;
         _wopt.workload_size = g_dbsize;
-        _wopt.num_threads = 1; // WE ONLY USE ONE THREAD TO WARM UP
+        _wopt.num_threads = g_num_threads; // WE ONLY USE ONE THREAD TO WARM UP
         start_workload(&_wopt);
     }
 
