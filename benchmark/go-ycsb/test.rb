5fc3f79b84190beeb53042e14cc68b083eaa3553
KinderRiven <1339764596@qq.com>
2021-06-14 16:01:28 +0800

    push by hsk

--- benchmark/go-ycsb/test.rb
+++ benchmark/go-ycsb/test.rb
@@ -1,4 +1,4 @@
-puts "This is a test ruby script!";
+puts "This is a Ruby Script!";
 
 bs = [64, 256, 1024, 4096, 16384]
 db = ["leveldb", "rocksdb", "matrixkv", "pebblesdb"]
@@ -6,6 +6,7 @@ db = ["leveldb", "rocksdb", "matrixkv", "pebblesdb"]
 bs.each do |e_bs|
     puts e_bs
     db.each do |e_db|
-        puts e_db
+        run = e_db + "_tester";
+        puts run
     end
 end
