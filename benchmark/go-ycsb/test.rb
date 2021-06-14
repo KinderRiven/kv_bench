990eab5e06260f0b13bdb7571783608705ce8bdc
KinderRiven <1339764596@qq.com>
2021-06-14 16:03:45 +0800

    push by hsk

--- benchmark/go-ycsb/test.rb
+++ benchmark/go-ycsb/test.rb
@@ -1,10 +1,10 @@
 puts "This is a Ruby Script!";
 
-size = 100 * 1024 * 1024 * 1024;
-bs = [64, 256, 1024, 4096, 16384]
+size = 100; # GB
+bs = [64, 256, 1024, 4096, 16384] # Byte
 db = ["leveldb", "rocksdb", "matrixkv", "pebblesdb"]
 
-puts size;
+puts "DB Size" + "[" + size + "GB]";
 
 bs.each do |e_bs|
     puts e_bs
